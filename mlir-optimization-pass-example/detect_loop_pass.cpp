897a77ae8c3369e1454b60e4efddab36e0ec8261
db7894 <dbashir@hmc.edu>
2024-07-02 21:45:56 -0700

    Remove commented code

--- mlir-optimization-pass-example/detect_loop_pass.cpp
+++ mlir-optimization-pass-example/detect_loop_pass.cpp
@@ -14,7 +14,6 @@ struct DetectLoopPass
       if (auto forOp = llvm::dyn_cast<mlir::scf::ForOp>(op)) {
         llvm::errs() << "Found a loop in function " << f.getName() << ": "
                      << forOp << "\n";
-        // Here you would implement your loop optimization logic
       }
     });
   }
@@ -23,46 +22,3 @@ struct DetectLoopPass
 std::unique_ptr<mlir::Pass> createDetectLoopPass() {
   return std::make_unique<DetectLoopPass>();
 }
-
-// struct DetectLoopPass
-//     : public mlir::PassWrapper<DetectLoopPass,
-//     mlir::OperationPass<mlir::ModuleOp>>
-// {
-//     void runOnOperation() override
-//     {
-//         mlir::ModuleOp module = getOperation();
-//         module.walk([&](mlir::Operation *op)
-//                     {
-//                         if (auto forOp =
-//                         llvm::dyn_cast<mlir::scf::ForOp>(op))
-//                         {
-//                             llvm::errs() << "Found a loop: " << forOp <<
-//                             "\n";
-//                         }
-//                     });
-//     }
-// };
-
-// std::unique_ptr<mlir::Pass> createDetectLoopPass()
-// {
-//     return std::make_unique<DetectLoopPass>();
-// }
-// struct DetectLoopPass
-//     : public mlir::PassWrapper<DetectLoopPass,
-//     mlir::OperationPass<mlir::func::FuncOp>>
-// {
-//     void runOnOperation() override
-//     {
-//         mlir::func::FuncOp f = getOperation();
-//         f.walk([&](mlir::scf::ForOp forOp)
-//                {
-//                    // Implement your loop optimization logic here
-//                    llvm::errs() << "Found a loop: " << forOp << "\n";
-//                });
-//     }
-// };
-
-// std::unique_ptr<mlir::Pass> createDetectLoopPass()
-// {
-//     return std::make_unique<DetectLoopPass>();
-// }
